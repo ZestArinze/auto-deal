15766bca4611ef52844b43c10fe589002c8fbaa4
Arinze Anakor <arinzeanakor@gmail.com>
2022-08-03 22:14:08 +0100

    chore(config): setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        // using sqlite for this demo
+        const options: TypeOrmModuleOptions = {
+          type: 'sqlite',
+          database: 'test.sqlite',
+          entities: [join(process.cwd(), 'src/**/*.entity.ts')],
+        };
+
+        return options;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
